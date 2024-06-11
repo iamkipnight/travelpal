9e832ea990696a091123917f6fcccfbb99317c6c
George Michael <georgemichaelkipkemboi@gmail.com>
2024-06-11 12:41:41 +0300

    TravDoc Doc Type added

---
+++ travelpal/travel_pal/doctype/travdoc/travdoc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, George Michael and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TravDoc', {
+	// refresh: function(frm) {
+
+	// }
+});
